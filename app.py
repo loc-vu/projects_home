db566db88ad1ca920d88cdb624716059e8eb5196
loc-vu <vhploc@gmail.com>
2024-11-01 23:21:00 -0700

    added initial multipage layout

--- app.py
+++ app.py
@@ -1,6 +1,16 @@
 import streamlit as st
-import pandas as pd
-import numpy as np
 
-st.title("Research Paper Recommender")
+def main():
+    home_page = st.Page("pages/home.py", title="Home", icon=":material/home:")
+    search_page = st.Page("pages/search.py", title="Paper Recommender", icon=":material/search:")
+    dashboard_page = st.Page("pages/dashboard.py", title="Arxiv Dashboard", icon=":material/dashboard:")
+    
+    pages = st.navigation([home_page, search_page, dashboard_page])
+    # st.set_page_config(page_title="Home")
+    pages.run()
 
+
+
+
+if __name__ == "__main__":
+    main()
---
+++ pages/search.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+
+st.title("Research Paper Recommender")
+search_bar = st.text_input("What research paper are you looking for?", value="")
