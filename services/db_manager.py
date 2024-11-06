658024f604e01e60f5507b2fdbb20d101950d4dd
loc-vu <vhploc@gmail.com>
2024-11-05 18:10:37 -0800

    adding skeleton for arxiv db

---
+++ services/db_manager.py
@@ -0,0 +1,17 @@
+import sqlite3
+
+class ArxivDB:
+    def __init__(self, db_path):
+        pass
+
+    def create_table(self):
+        pass
+
+    def insert_paper(self):
+        pass
+
+    def get_paper(self):
+        pass
+
+    def close(self):
+        pass
