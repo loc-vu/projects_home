f0a7e9595ce4ea60a912c7f8a5cf8c28b09a53e7
loc-vu <vhploc@gmail.com>
2024-11-07 20:30:22 -0800

    added function to create db and table

--- services/db_manager.py
+++ services/db_manager.py
@@ -2,10 +2,24 @@ import sqlite3
 
 class ArxivDB:
     def __init__(self, db_path):
-        pass
+        self.db_path = db_path
+        self.connection = sqlite3.connect(self.db_path)
+        self.cursor = self.connection.cursor()
 
-    def create_table(self):
-        pass
+    def create_table(self, table_name='papers'):
+        self.cursor.execute(f"""
+        CREATE TABLE IF NOT EXISTS {table_name} (
+            id TEXT PRIMARY KEY,
+            title TEXT,
+            authors TEXT,
+            abstract TEXT,
+            categories TEXT,
+            journal_ref TEXT,
+            doi TEXT
+        )
+        """)
+
+        self.connection.commit()
 
     def insert_paper(self):
         pass
@@ -15,3 +29,10 @@ class ArxivDB:
 
     def close(self):
         pass
+
+
+if __name__ == '__main__':
+    arxiv_db = ArxivDB(db_path=r'C:\Users\lukev\Documents\projects\projects_home\data\arxiv_papers.db')
+    arxiv_db.create_table(
+        table_name="papers"
+    )
