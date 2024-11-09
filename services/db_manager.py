44e84fd903b332a9947b001e6b702c7f763e577f
loc-vu <vhploc@gmail.com>
2024-11-08 17:52:52 -0800

    added insert paper, insert bulk, and close connection function

--- services/db_manager.py
+++ services/db_manager.py
@@ -1,4 +1,5 @@
 import sqlite3
+from typing import List, Dict
 
 class ArxivDB:
     def __init__(self, db_path):
@@ -6,7 +7,10 @@ class ArxivDB:
         self.connection = sqlite3.connect(self.db_path)
         self.cursor = self.connection.cursor()
 
-    def create_table(self, table_name='papers'):
+    def __del__(self):
+        self.close()
+
+    def create_table(self, table_name: str ='papers'):
         self.cursor.execute(f"""
         CREATE TABLE IF NOT EXISTS {table_name} (
             id TEXT PRIMARY KEY,
@@ -21,18 +25,43 @@ class ArxivDB:
 
         self.connection.commit()
 
-    def insert_paper(self):
-        pass
+    def insert_paper_bulk(self, table_name: str, papers: List[Dict]):
+        for p in papers:
+            self.insert_paper(table_name, p)
+
+    def insert_paper(self, table_name: str, paper: Dict):
+        self.cursor.execute(f"""
+            INSERT OR IGNORE INTO {table_name}        
+                (id, title, authors, abstract, categories, journal_ref, doi)
+            VALUES
+                (?, ?, ?, ?, ?, ?, ?)
+        """, (
+            paper['id'],
+            paper['title'],
+            paper['authors'],
+            paper['abstract'],
+            paper['categories'],
+            paper['journal_ref'],
+            paper['doi'],
+        ))
+
+        self.connection.commit()
 
     def get_paper(self):
         pass
 
+    def print_table(self, table_name):
+        self.cursor.execute(f"SELECT * FROM {table_name}")
+        print(self.cursor.fetchall())
+
     def close(self):
-        pass
+        self.connection.close()
 
 
-if __name__ == '__main__':
-    arxiv_db = ArxivDB(db_path=r'C:\Users\lukev\Documents\projects\projects_home\data\arxiv_papers.db')
-    arxiv_db.create_table(
-        table_name="papers"
-    )
+# if __name__ == '__main__':
+#     arxiv_db = ArxivDB(
+#         db_path=r'C:\Users\lukev\Documents\projects\projects_home\data\arxiv_papers.db'
+#     )
+#     arxiv_db.create_table(
+#         table_name="papers"
+#     )
